3f14bbfe5d848750e4c5a6e03c8440dddac5b278
Jacobs David <jacobsdavid.dr@gmail.com>
2026-02-12 13:26:31 +0100

    03: authentication

--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -8,6 +8,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useAuth, useClerk } from "@clerk/nextjs";
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -16,20 +17,25 @@ const items = [
     title: "History",
     url: "/playlists/history",
     icon: HistoryIcon,
+    auth: true,
   },
   {
     title: "Liked videos",
     url: "/playlists/liked",
     icon: ThumbsUpIcon,
+    auth: true,
   },
   {
     title: "All Playlists",
     url: "/playlists",
     icon: ListVideoIcon,
+    auth: true,
   },
 ];
 
 export const PersonalSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
   return (
     <SidebarGroup>
       <SidebarGroupLabel>You</SidebarGroupLabel>
@@ -40,8 +46,13 @@ export const PersonalSection = () => {
               <SidebarMenuButton
                 tooltip={item.title}
                 asChild
-                // isActive={false} TODO: Change to look at current pathname
-                onClick={() => {}} //TODO: Chnage to do something
+                isActive={false} //TODO: Change to look at current pathname
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />

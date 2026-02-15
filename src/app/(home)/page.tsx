735e4504c9bfbd8209561e991701726a11f84ce1
Jacobs David <jacobsdavid.dr@gmail.com>
2026-02-15 11:34:03 +0100

    08: video-categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,23 @@
+// app/page.tsx
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { Suspense } from "react";
-import { PageClient } from "./client";
-import { ErrorBoundary } from "react-error-boundary";
 
-export default async function Home() {
-  await trpc.hello.prefetch({ text: "Jacobs" });
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
 
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Error</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
-}
+};
+
+export default Page;
---
+++ src/scripts/seed-categories.ts
@@ -0,0 +1,36 @@
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames = [
+  "Cars and vehicles",
+  "Comedy",
+  "Education",
+  "Gaming",
+  "Entertainment",
+  "Film and animation",
+  "How-to and style",
+  "Music",
+  "News and politics",
+  "People and animals",
+  "Science and technology",
+  "Sports",
+  "travel and events",
+];
+
+async function main() {
+  console.log("Seeding categories");
+  try {
+    const values = categoryNames.map((name) => ({
+      name,
+      description: `Videos related to ${name.toLowerCase()}`,
+    }));
+
+    await db.insert(categories).values(values);
+    console.log("Categories seeded succssfully");
+  } catch (error) {
+    console.error("Error seeding categories", error);
+    process.exit(1);
+  }
+}
+
+main();

f8cab6afa74eef5280586a54098290b5ae402567
Jacobs David <jacobsdavid.dr@gmail.com>
2026-02-16 14:23:22 +0100

    10: studio-videos

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -2,12 +2,13 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { AuthButton } from "@/modules/auth/ui/components/auth-button";
 import Image from "next/image";
 import Link from "next/link";
+import { StudioUploadModal } from "../studio-upload-modal";
 
 export const StudioNavbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50 border-b shadow-md">
       <div className="flex items-center gap-4 w-full">
-        {/* Menu and Lofgo */}
+        {/* Menu and Logo */}
         <div className="flex items-center flex-shrink-0">
           <SidebarTrigger />
           <Link href="/studio">
@@ -29,6 +30,7 @@ export const StudioNavbar = () => {
 
         {/* Auth Button */}
         <div className="flex-shrink-0 items-center flex gap-4">
+          <StudioUploadModal />
           <AuthButton />
         </div>
       </div>

bcd154afe35141c56cef94d4acdcf8a812ca7196
Jacobs David <jacobsdavid.dr@gmail.com>
2026-02-27 14:13:37 +0100

    28: improvements

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const DEFAULT_LIMIT = 2;
 
 export const THUMBNAIL_FALLBACK = "/placeholder.svg";
-export const APP_URL = process.env.VERCEL_URL;
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,8 +1,14 @@
 import { db } from "@/db";
-import { videos } from "@/db/schema";
+import {
+  comments,
+  users,
+  videoReactions,
+  videos,
+  videoViews,
+} from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, desc, eq, lt, or } from "drizzle-orm";
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { z } from "zod";
 export const studioRouter = createTRPCRouter({
   getOne: protectedProcedure
@@ -43,8 +49,24 @@ export const studioRouter = createTRPCRouter({
       const { id: userId } = ctx.user;
 
       const data = await db
-        .select()
+
+        .select({
+          ...getTableColumns(videos),
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          commentCount: db.$count(
+            comments,
+            and(eq(comments.videoId, videos.id), eq(comments.isDeleted, false)),
+          ),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like"),
+            ),
+          ),
+        })
         .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
         .where(
           and(
             eq(videos.userId, userId),
